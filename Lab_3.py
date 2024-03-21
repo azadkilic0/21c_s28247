827e97b3fe8a2d11484962af323928aed0bb233b
azadkilic0 <a.kilic1213@gmail.com>
2024-03-21 11:59:43 +0100

    Task-3 Completed

--- Lab_3.py
+++ Lab_3.py
@@ -8,3 +8,14 @@ def generate_squares(start, end):
 # Example usage of the function
 example_squares = generate_squares(1, 10)
 print("Task 2 :",example_squares)
+
+#Task-3
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        """Generate a list of squares for a given range of numbers."""
+        return [x**2 for x in range(start, end+1)]
+# Example usage:
+if __name__ == "__main__":
+    square_generator = SquareGenerator()
+    squares = square_generator.generate_squares(1, 10)
+    print("Task 3 :",squares)
