6c7b37d809b08ca6e0c3d18277e21825697f497e
azadkilic0 <a.kilic1213@gmail.com>
2024-03-21 12:26:43 +0100

    Task-9 Completed

--- square_generator_pkg/SquareGenerator.py
+++ square_generator_pkg/SquareGenerator.py
@@ -1,6 +1,8 @@
 class SquareGenerator:
     def generate_squares(self, start, end):
         """Generate a list of squares for a given range of numbers."""
+        if start > end:
+            raise ValueError("Start of range must be less than or equal to the end.")
         return [x ** 2 for x in range(start, end + 1)]
 
 class CubicGenerator(SquareGenerator):
